61a5c8ab2d1e4e80acfa5eeb314d70b32e7876ef
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2025-03-22 15:51:33 +0000

    Minor code change

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard"; 
-import ContactForm from "./Contacts";
+import Contacts from "./Contacts";
 
 const MainContent: React.FC = () => {
   return (
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/contacts" element={<ContactForm />} />
+        <Route path="/contacts" element={<Contacts />} />
       </Routes>
     </main>
   );
