cd67df613051f540b0eea7838fee6ff7554acc57
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2025-03-22 15:41:08 +0000

    Fixed issue due to merge

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useEffect, useState } from "react"; 
-import { db, storage, collection, addDoc, ref, uploadBytes, 
-  getDownloadURL, setDoc, getDocs, updateDoc, doc, getDoc  } from "./api/firebase.ts";
+import { db } from "../src/api/firebase";
+import { Routes, Route } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore"
+import Dashboard from "./Dashboard"; 
+import ContactForm from "./Contacts";
 
 //const users: User[] = [
 //  { name: "Florence Shaw", email: "florence@example.com", joined: "Mar 4, 2024" },
@@ -18,30 +21,6 @@ interface Contact {
 }
 
 const MainContent: React.FC = () => {
-
-const [contacts, setContacts] = useState<Contact[]>([]);
-
-useEffect(() => {
-  fetchContacts();
-}, []);
-
- const fetchContacts = async () => {
-    // Reference to the "contacts" collection
-    const contactsRef = collection(db, "Contacts");
-    const snapshot = await getDocs(contactsRef);
-
-    // Map through the documents to extract each contact's id and data
-    const contactsList: Contact[] = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...(doc.data() as Omit<Contact, "id">),
-    }));
-
-    // Update state with the fetched contacts
-    setContacts(contactsList);
-  };
-
-  console.log(contacts);
-
   return (
     <main className="main-content">
       <Routes>
