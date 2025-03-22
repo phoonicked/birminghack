f37fbe930fc4d99f4ae40a3e1e8ed50ed70f620d
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2025-03-22 15:49:46 +0000

    Removed unused code

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { useEffect, useState } from "react"; 
-import { db } from "../src/api/firebase";
 import { Routes, Route } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore"
 import Dashboard from "./Dashboard"; 
 import ContactForm from "./Contacts";
-
-//const users: User[] = [
-//  { name: "Florence Shaw", email: "florence@example.com", joined: "Mar 4, 2024" },
-//  { name: "Amelie Laurent", email: "amelie@example.com", joined: "Mar 5, 2024" },
-//  { name: "Sienna May", email: "sienna@example.com", joined: "Mar 7, 2024" },
-//];
-
-interface Contact {
-  id: string;
-  name: string;
-  telephone: string;
-  image: string;
-  time: string;
-}
 
 const MainContent: React.FC = () => {
   return (
